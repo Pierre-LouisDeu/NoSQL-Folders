f9bab516129badad88c1123ad0f396530609d47f
Pierre-LouisDeu <73912685+Pierre-LouisDeu@users.noreply.github.com>
2022-10-07 01:36:11 +0200

    fix: delete folder and child n-1

--- hooks/useDelete.ts
+++ hooks/useDelete.ts
@@ -13,18 +13,17 @@ const useDelete = () => {
   // Delete all documents in the collection folders that contains the parent id in the parent field
   const deleteFolder = async (parent : parentType, id: string) => {
     const key = parent + '_' + id
-    // const key = parent
-    console.log({key})
     const query = await firebase.db
       .collection("folders")
       .where("parent", ">=", key)
       .where("parent", "<=", `${key}_`) 
       .get();
     query.forEach((doc) => {
-    //   doc.ref.delete();
+      doc.ref.delete();
     const title = doc.data().title;
     console.log('delete :',doc.id, {title});
     });
+    await firebase.db.collection("folders").doc(id).delete();
     setReload(true);
   };
 
